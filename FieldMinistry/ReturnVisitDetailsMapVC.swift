4a8cc1eb37ffcae090f332f70dbfc207b8eb8e51
J0na17anT98 <pilotj@outlook.com>
2017-05-10 10:03:24 -0400

    Some more changes
    added some assets, added location button next to a persons address so
    it is easy to route to the house! :)

---
+++ FieldMinistry/ReturnVisitDetailsMapVC.swift
@@ -0,0 +1,39 @@
+//
+//  ReturnVisitDetailsMapVC.swift
+//  FieldMinistry
+//
+//  Created by Jonathan Tsistinas on 5/10/17.
+//  Copyright © 2017 Jonathan Tsistinas. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ReturnVisitDetailsMapVC: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
